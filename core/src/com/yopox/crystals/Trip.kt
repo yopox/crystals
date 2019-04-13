91ae731152bb239508c384295357b1a14c69f34d
yopox <louis.vignier@supelec.fr>
2019-04-13 15:39:52 +0200

    :art: Trip draw functions, separation lines

--- core/src/com/yopox/crystals/Trip.kt
+++ core/src/com/yopox/crystals/Trip.kt
@@ -1,5 +1,6 @@
 package com.yopox.crystals
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -11,8 +12,7 @@ import ktx.graphics.use
 /**
  * Trip Screen.
  *
- * TODO: Clean rectangle draw function
- * TODO: Icons separation lines
+ * TODO: Real chunk generation
  */
 class Trip : KtxScreen {
     private val batch = SpriteBatch()
@@ -27,29 +27,14 @@ class Trip : KtxScreen {
         batch.projectionMatrix = camera.combined
         shapeRenderer.projectionMatrix = camera.combined
 
-        Util.drawRect(shapeRenderer, 8f, 8f, Util.WIDTH / 4, Util.HEIGHT / 2)
-
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 2 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 3 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 4 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
-
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 24f, 8f, 36f, 16f)
-        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 32f + 36f, 8f, 36f, 16f)
-
+        // Draw the title
         batch.use {
             Util.bigFont.draw(it, Util.TEXT_TRIP, 10f, Util.HEIGHT - 10)
-            Util.font.draw(it, "Bag", Util.WIDTH / 4 + 24f + posX1, 19f)
-            Util.font.draw(it, "Options", Util.WIDTH / 4 + 32f + 36f + posX2, 19f)
-
-            for (i in 0..4) {
-                for (j in 0..2) {
-                    it.draw(icons, Util.WIDTH / 4 + 16f + 20*i + 1, Util.HEIGHT / 3 + 1 + 15*j, 14 * Crystals.events[i][j], 0, 14, 14)
-                }
-            }
         }
 
+        drawStatus()
+        drawChunks()
+        drawButtons()
     }
 
     override fun resize(width: Int, height: Int) {
@@ -60,6 +45,47 @@ class Trip : KtxScreen {
 
     override fun dispose() {
         batch.dispose()
+    }
+
+    private fun drawStatus() {
+        Util.drawRect(shapeRenderer, 8f, 8f, Util.WIDTH / 4, Util.HEIGHT / 2)
+    }
+
+    private fun drawChunks() {
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 2 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 3 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 16f + 4 * 20f, Util.HEIGHT / 3, 16f, 3 * 14 + 4f)
+
+        batch.use {
+            for (i in 0..4) {
+                for (j in 0..2) {
+                    it.draw(icons, Util.WIDTH / 4 + 16f + 20 * i + 1, Util.HEIGHT / 3 + 1 + 15 * j, 14 * Crystals.events[i][j], 0, 14, 14)
+                }
+            }
+        }
+
+        shapeRenderer.use(ShapeRenderer.ShapeType.Filled) {
+            it.color = Color.WHITE
+            for (i in 0..4) {
+                for (j in 1..2) {
+                    val x = Util.WIDTH / 4 + 16f + 20 * i + 1
+                    val y = Util.HEIGHT / 3 + 15 * j
+                    it.rect(x, y, 14f, 1f)
+                }
+            }
+        }
+    }
+
+    private fun drawButtons() {
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 24f, 8f, 36f, 16f)
+        Util.drawRect(shapeRenderer, Util.WIDTH / 4 + 32f + 36f, 8f, 36f, 16f)
+
+        batch.use {
+            Util.font.draw(it, "Bag", Util.WIDTH / 4 + 24f + posX1, 19f)
+            Util.font.draw(it, "Options", Util.WIDTH / 4 + 32f + 36f + posX2, 19f)
+        }
     }
 
 }
